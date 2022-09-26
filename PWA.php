a0fe8237f3ddee5e2f28e49e4313b113618514df
Antoine Mercier-Linteau <antoine@bitsofmymind.com>
2022-09-26 12:59:53 -0400

    Identify the current PWA with a URL parameter

--- PWA.php
+++ PWA.php
@@ -34,79 +34,114 @@ class PWA {
 		// Register some JS and CSS for standalone mode. This code should be in the service worker but until I get a better grasp of how they work is will be included in every page.
 		$out->addModuleStyles('ext.PWA.standalone.css'); // Add the CSS before the JS is loaded.
 		$out->addModules('ext.PWA.standalone.js'); // This will add the JS.
+		
+		// If this request was made within the context of a PWA.
+		if($PWAId = $out->getRequest()->getQueryValuesOnly()['pwa-id']) {
 
-		// Loop over all configured PWAs to check which one we should use for the requested page.
-		foreach ($globalConfig->get( 'PWAConfigs' ) as $id => $config){
-			if(!$config) { continue; } // If that PWA has been turned off (useful for disabling the default PWA [since the provide_default merge strategy only works in MW > 1.35.3]).
+			/* Figuring out if a PWA is in use is easy, we just check in JS if the navigator in is standalone mode.
+			 * Figuring out which PWA is in use is hard for the following reasons:
+			 * - there is no JS call that will let us know which manifest is in use
+			 * - cookies are shared between all PWAs and broswer tabs
+			 * - indexDB and the Web Storage API are persisted over a domain
+			 * - ServiceWorkers work within a domain over scope (if two PWAs share the same scope, it does not work).
+			 * 
+			 * What remains is trying to do it with URL. To that effet, a pwa-id parameter is provided in the start_url of the manifest and it is then added to every subsequent URL. */
 
-			// Format the $pattern parameter.
-			if(!isset($config['patterns'])) {
-				wfDebugLog( 'PWA', "$id PWA missing the pattern configuration entry." );
-				continue; // Misconfigured property, skip this PWA.
-			} else {
-				// Make sure patterns is an array.
-				$patterns = $config['patterns'];
-				$patterns = is_string($patterns) ? [$patterns]: $patterns;
+			$configs = $globalConfig->get( 'PWAConfigs' ); 
+			if(!isset($configs[$PWAId])) { 
+
+				/* The id could not be found, maybe the user modified the URL manually ? 
+				 * Show an error page and prompt the user to close the PWA and re-open it (thus inserting the pwa-id in the start_url again). */
+				wfDebugLog ('PWA', 'Invalid PWA id supplied in the URL parameter.'); 
+				$out->showErrorPage(wfMessage('PWA-invalid-pwa-id-title'), wfMessage('PWA-invalid-pwa-id-message'));
+
+				return;
 			}
 
-			$title = $out->getTitle()->getFullText();
+			// Retrieve the manifest for this PWA.
+			$manifest = json_decode(wfMessage($configs[$PWAId]['manifest'])->text());
 
-			// Check if the current page's title matches the pattern.
-			foreach($patterns as $pattern)
-			{
-				$pattern = str_replace('_', ' ', $pattern); // Space and _ are considered equivalent in page titles, Title does no include _ in page names.
-				
-				if(!preg_match($pattern, $title)) { continue; }
-				// The PWA matches the title.
-		
-				if(!isset($config['manifest'])) {
-					wfDebugLog( 'PWA', "$id PWA missing the manifest configuration entry." );
-					continue 2; // Misconfigured property, skip this PWA.
+			// Expose the start_url parameter.
+			$out->addJsConfigVars('wgCurrentPWAStartUrl', $manifest->start_url);
+			// Expose the PWA id trough js (probably safer than to retrieve it from the URL client side).
+			$out->addJsConfigVars('wgCurrentPWAId', $PWAId);
+
+
+		} else { // This request was not made within the context of a PWA, present the user with all the code required to install the PWA.
+
+			// Loop over all configured PWAs to check which one we should use for the requested page.
+			foreach ($globalConfig->get( 'PWAConfigs' ) as $id => $config){
+				if(!$config) { continue; } // If that PWA has been turned off (useful for disabling the default PWA [since the provide_default merge strategy only works in MW > 1.35.3]).
+
+				// Format the $pattern parameter.
+				if(!isset($config['patterns'])) {
+					wfDebugLog( 'PWA', "$id PWA missing the pattern configuration entry." );
+					continue; // Misconfigured property, skip this PWA.
+				} else {
+					// Make sure patterns is an array.
+					$patterns = $config['patterns'];
+					$patterns = is_string($patterns) ? [$patterns]: $patterns;
 				}
 
-				$manifestUrl = $config['manifest'];
+				$title = $out->getTitle()->getFullText();
 
-				if(!\Title::newFromText($manifestUrl, NS_MEDIAWIKI)->exists()) { // If the manifest pointed to by the config was not defined or does not exist.
-					wfDebugLog( 'PWA', "$id PWA manifest does not exist." );
-					continue 2; // Skip that PWA.
+				// Check if the current page's title matches the pattern.
+				foreach($patterns as $pattern)
+				{
+					$pattern = str_replace('_', ' ', $pattern); // Space and _ are considered equivalent in page titles, Title does no include _ in page names.
+					
+					if(!preg_match($pattern, $title)) { continue; }
+					// The PWA matches the title.
+			
+					if(!isset($config['manifest'])) {
+						wfDebugLog( 'PWA', "$id PWA missing the manifest configuration entry." );
+						continue 2; // Misconfigured property, skip this PWA.
+					}
+
+					$manifestUrl = $config['manifest'];
+
+					if(!\Title::newFromText($manifestUrl, NS_MEDIAWIKI)->exists()) { // If the manifest pointed to by the config was not defined or does not exist.
+						wfDebugLog( 'PWA', "$id PWA manifest does not exist." );
+						continue 2; // Skip that PWA.
+					}
+
+					$manifest = json_decode(wfMessage($manifestUrl)->text());
+
+					$manifestUrl = $globalConfig->get( 'ScriptPath' ) .'/index.php?title=MediaWiki:'.urlencode($manifestUrl).'&action=raw&ctype=text/json';
+
+					$out->addHeadItem('pwa', '<link rel="manifest" href="'.$manifestUrl.'" data-PWA-id="'.htmlspecialchars($id).'" />');
+
+					$icons = $manifest->icons ?? [];
+
+					if ( $icons[0] ?? false ) {
+						$icon = $icons[0]->src;
+
+						// Set the apple-touch-icon (because iOS ignores the icon field in the manifest). Add it as a head item to avoid modification by other extensions.
+						$out->addHeadItem('apple-touch-icon', '<link rel="apple-touch-icon" href="'.$icon.'" />');	
+						// All other apple-touch-icon link tags will be removed later on in onOutputPageAfterGetHeadLinksArray.
+					}
+					
+					// Register the add-to-homescreen JS/CSS module.
+					$out->addModules('ext.PWA.add-to-homescreen');
+
+					// Register the PWA extension's JS which will then register the service worker.
+					$out->addModules('ext.PWA');
+
+					// Pass config parameters to mw.config so it can be fetched in JS.
+					$out->addJsConfigVars('wgCurrentInstallablePWAId', $id);
+					$pwaname = $manifest->name ?? null;
+					if ( $pwaname ) {
+						$out->addJsConfigVars('wgCurrentInstallablePWAName', $pwaname);
+					}
+
+					// Add some more metas.
+					$out->addHeadItem('mobile-web-app-capable', '<meta name="mobile-web-app-capable" content="yes" />');
+					$out->addHeadItem('apple-mobile-web-app-capable', '<meta name="apple-mobile-web-app-capable" content="yes" />');
+					$out->addHeadItem('application-name', '<meta name="application-name" content="'.$pwaname ?? $id.'">');
+					$out->addHeadItem('apple-mobile-web-app-title', '<meta name="apple-mobile-web-app-title" content="'.$pwaname ?? $id.'">');
+
+					return; // Skip all other PWA configurations.
 				}
-
-				$manifest = json_decode(wfMessage($manifestUrl)->text());
-
-				$manifestUrl = $globalConfig->get( 'ScriptPath' ) .'/index.php?title=MediaWiki:'.urlencode($manifestUrl).'&action=raw&ctype=text/json';
-
-				$out->addHeadItem('pwa', '<link rel="manifest" href="'.$manifestUrl.'" data-PWA-id="'.htmlspecialchars($id).'" />');
-
-				$icons = $manifest->icons ?? [];
-
-				if ( $icons[0] ?? false ) {
-					$icon = $icons[0]->src;
-
-					// Set the apple-touch-icon (because iOS ignores the icon field in the manifest). Add it as a head item to avoid modification by other extensions.
-					$out->addHeadItem('apple-touch-icon', '<link rel="apple-touch-icon" href="'.$icon.'" />');	
-					// All other apple-touch-icon link tags will be removed later on in onOutputPageAfterGetHeadLinksArray.
-				}
-				
-				// Register the add-to-homescreen JS/CSS module.
-				$out->addModules('ext.PWA.add-to-homescreen');
-
-				// Register the PWA extension's JS which will then register the service worker.
-				$out->addModules('ext.PWA');
-
-				// Pass config parameters to mw.config so it can be fetched in JS.
-				$out->addJsConfigVars('wgCurrentInstallablePWAId', $id);
-				$pwaname = $manifest->name ?? null;
-				if ( $pwaname ) {
-					$out->addJsConfigVars('wgCurrentInstallablePWAName', $pwaname);
-				}
-
-				// Add some more metas.
-				$out->addHeadItem('mobile-web-app-capable', '<meta name="mobile-web-app-capable" content="yes" />');
-				$out->addHeadItem('apple-mobile-web-app-capable', '<meta name="apple-mobile-web-app-capable" content="yes" />');
-				$out->addHeadItem('application-name', '<meta name="application-name" content="'.$pwaname ?? $id.'">');
-				$out->addHeadItem('apple-mobile-web-app-title', '<meta name="apple-mobile-web-app-title" content="'.$pwaname ?? $id.'">');
-
-				return; // Skip all other PWA configurations.
 			}
 		}
 	}
@@ -128,7 +163,7 @@ class PWA {
 	public static function onRecentChangeSave( \RecentChange &$recentChange ) {
 		global $wgRequest;
 
-		if($PWAId = $wgRequest->getCookie("PWAId", '')) { // If this change was made with a PWA.
+		if($PWAId = $out->getRequest()->getQueryValuesOnly()['pwa-id']) { // If this change was made with a PWA.
 			$recentChange->addTags(['PWA-edit', 'PWA-edit-'.$PWAId]);
 		}
 
@@ -162,6 +197,21 @@ class PWA {
 			// If this tag defines an apple-touch-icon.
 			if(strpos($t, 'apple-touch-icon')) { 
 				unset($tags[$k]); // Delete it.
+			}
+		}
+	}
+
+	/** 
+	 * If the pwa-id URL parameter was part of the request, propagate it.
+	 * */
+	public static function onBeforePageRedirect( $out, &$redirect, &$code ) { 
+		if($PWAId = $out->getRequest()->getQueryValuesOnly()['pwa-id']) {
+			$url = parse_url($redirect);
+
+			if($url['query']) { // If the url had a query.
+				$redirect .= '&pwa-id='.$PWAId; // Append to it.
+			} else {
+				$redirect .= '?pwa-id='.$PWAId; // Add a new query part.
 			}
 		}
 	}
--- resources/ext.PWA.standalone/PWA.standalone.js
+++ resources/ext.PWA.standalone/PWA.standalone.js
@@ -8,10 +8,12 @@ if(navigator.standalone || // Safari
         $('body').append('<div id="pwa-loader" onclick="$(this).hide();"></div>');
     });
 
-    /* Tell the server we are in PWA mode and which PWA is in use (so changes can be tagged accordingly in RecentChanges or usage tracked in site stats).
-     * Use a cookie for that. The WebRequest API (https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Intercept_HTTP_requests)
-     * would be ideal, but not properly supported by Apple. */
-    document.cookie = 'PWAId=' + mw.config.get("wgCurrentPWAId") + ';path=/ ;max-age=3600';
+    var id = mw.config.get("wgCurrentPWAId");
+
+    // No id was provided by the server, maybe the user modified the URL manually?
+    if(!id) { 
+        return; // Let the user proceed, the pwa-id will be reinjected in the url when user closes and reopen the app.
+    }
 
     /* If the main page differs from the start_url (as stated in the manifest, replace links to the main page with links to the start_url. */
     var currentMainPagePath = mw.config.get('wgArticlePath').replace('$1', mw.message('mainpage'));
@@ -20,7 +22,45 @@ if(navigator.standalone || // Safari
     if(currentMainPagePath != start_url) { // If the URLs do not match.
         $('a[href="' + currentMainPagePath + '"]').attr('href', start_url); // Redirect all links to the main page to start_url.
     }
+
+    /*
+     * Adds the current PWA id to a URL. 
+     */
+    function injectPWAId(href) {
+        
+        if(href[0] == '#') { return; } // Do nothing on fragments.
+        if(href.indexOf('javascript') == 0) { return; } // Do nothing on JS code.
+        if(href.indexOf('mailto') == 0) { return; } // Do nothing on mailto links.
+
+        if(href[href.length - 1] == '/' && href.indexOf('?') == -1) { href = href + '?pwa-id=' + id; }
+        else {
+
+            // Make sure the protocol and hostname are part of the url (otherwise the URL object will throw an error).
+            if(href.indexOf(window.location.protocol) != 0) { href = window.location.protocol + '//' + window.location.hostname + href; }
+            
+            try { href = new URL(href); }
+            catch (e) {
+                console.log(href + " cannot be processed.")
+                throw e;
+            }
+
+            href.searchParams.set('pwa-id', id);
+        }
+
+        return href;
+    };
+
+    /* From one page request to the next, there must be a way for us to know the user is using which PWA.
+     * Use a URL parameter for that effet and make sure it's always added to the next URL. */
+    $('a').click(function(e) {
+       $(this).attr('href', injectPWAId($(this).attr("href")));
+    });
+
+    // Do the same with form actions.
+    $('form').each(function() { 
+        $(this).attr('action', injectPWAId($(this).attr("action"))); 
+    });
 }
 else { // Not in PWA mode.
-    document.cookie = 'PWAId=' + mw.config.get("wgCurrentPWAId") + ';path=/ ;max-age=0'; // Delete cookie.
+    
 }
